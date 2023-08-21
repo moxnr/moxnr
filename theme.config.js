7c0d0109760bcb81307d53b7ab73709ae38a569d
maxnerman <maxnerman@pm.me>
2023-08-21 15:57:20 +0200

    init portfolio

---
+++ theme.config.js
@@ -0,0 +1,18 @@
+const YEAR = new Date().getFullYear()
+
+export default {
+  footer: (
+    <footer>
+      <small>
+      </small>
+      <style jsx>{`
+        footer {
+          margin-top: 8rem;
+        }
+        a {
+          float: right;
+        }
+      `}</style>
+    </footer>
+  ),
+}
